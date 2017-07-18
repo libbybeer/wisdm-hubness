f5a6dfffe52b407cb017020c69666e406ae898ae
libbybeer <libby.beer@gmail.com>
2017-07-18 14:44:07 -0400

    fixed plot so that colors are correct

--- icerm.py
+++ icerm.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 np.random.seed(2017) # set seed for reproducible results
 
-N = 100 # number of data points
+N = 1000 # number of data points
 d = 10 # number of dimensions
 
 xmean = np.zeros(d)
@@ -22,7 +22,7 @@ allx = np.array([np.random.normal(xmean) for j in range(N)])
 ally = np.array([np.random.normal(ymean) for j in range(N)])
 alldata10 = np.concatenate((allx,ally))
 
-N = 100 # number of data points per cluster
+N = 1000 # number of data points per cluster
 d = 100 # number of dimensions
 
 xmean = np.zeros(d)
@@ -51,30 +51,39 @@ def hubness(i,nearest_neighbors,k):
     #i = np.where(allx==x)
     return len(np.where(nearest_neighbors.T[1:(k+1)]==i)[0])
 
-dist10 = distances(alldata10)
-nn10 = nearest_neighbors(dist10)
-hubness10 = [hubness(i,nn10,3) for i in range(len(alldata10))]
+def dist_nn_hubness(allx,k):
+    d = distances(allx)
+    nn = nearest_neighbors(d)
+    h = np.array([hubness(i,nn,k) for i in range(len(allx))])
+    return [d,nn,h]
+    
+
+#dist10 = distances(alldata10)
+#nn10 = nearest_neighbors(dist10)
+#hubness10 = [hubness(i,nn10,3) for i in range(len(alldata10))]
+[dist10,nn10,hubness10] = dist_nn_hubness(alldata10,3)
 
 plt.interactive(True)
 
 plt.figure(0)
-plt.scatter(alldata10.T[0],alldata10.T[1],
-            c=hubness10,
-            cmap="copper",s=60)
+plt.scatter(alldata10.T[0][list(hubness10.argsort())],
+            alldata10.T[1][list(hubness10.argsort())],
+            c=hubness10[hubness10.argsort()], cmap="copper",s=60,linewidth=0)
 
 plt.figure(1)
 plt.hist(hubness10)
 
-dist100 = distances(alldata100)
-nn100 = nearest_neighbors(dist100)
-hubness100 = [hubness(i,nn100,3) for i in range(len(alldata100))]
+#dist100 = distances(alldata100)
+#nn100 = nearest_neighbors(dist100)
+#hubness100 = [hubness(i,nn100,3) for i in range(len(alldata100))]
+[dist100,nn100,hubness100] = dist_nn_hubness(alldata100,3)
 plt.figure(2)
 plt.hist(hubness100)
 
 plt.figure(3)
-plt.scatter(alldata100.T[0],alldata100.T[1],
-            c=hubness100,
-            cmap="copper",s=60)
+plt.scatter(alldata100.T[0][list(hubness100.argsort())],
+            alldata100.T[1][list(hubness100.argsort())],
+            c=hubness100[hubness100.argsort()], cmap="copper",s=60,linewidth=0)
 
 
 end = time.time()
